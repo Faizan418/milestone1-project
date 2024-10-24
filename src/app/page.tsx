fc20cfd753528572b6e94088bd09a40136b99578
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-10-25 00:10:35 +0500

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center p-14 flex-col align-middle bg-yellow-200">
+      <h2 className="font-bold">Hello Word</h2>
+
+      <h1 className="text-center font-bold ">Commit Soon...</h1>
+    </div>
+  );
+}
